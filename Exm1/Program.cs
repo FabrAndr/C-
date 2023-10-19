ed5ab53312dd28f607af1987f5e1a2525490e200
Fabr <fabrics.andrey@yandex.ru>
2023-10-20 00:54:51 +0300

    ура, это маша

--- Exm1/Program.cs
+++ Exm1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int numberA = 31;
+int numberA = new Random().Next(1, 10);
 int numberB = 5;
 int result = numberA + numberB;
 Console.WriteLine(result);
--- Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
